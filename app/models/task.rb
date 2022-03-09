faa7703dd71ebd599fdbe16a1cba6e0cd870af07
Eswar Tallapudi <eswar.tallapudi@bigbinary.com>
2022-03-09 12:25:46 +0530

    Added association in User model for assigned tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  belongs_to :assigned_user, foreign_key: "assigned_user_id", class_name: "User"
   MAX_TITLE_LENGTH = 125
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
   validates :slug, uniqueness: true
