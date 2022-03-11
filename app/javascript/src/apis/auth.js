2a31cf65caaaa54893866137696902c0de60ad24
Eswar Tallapudi <eswar.tallapudi@bigbinary.com>
2022-03-11 11:36:48 +0530

    Added ability to logout

--- app/javascript/src/apis/auth.js
+++ app/javascript/src/apis/auth.js
@@ -4,9 +4,12 @@ const signup = payload => axios.post("/users", payload);
 
 const login = payload => axios.post("/session", payload);
 
+const logout = () => axios.delete("/session");
+
 const authApi = {
   signup,
-  login
+  login,
+  logout
 };
 
 export default authApi;
